bf4ae982314b2d2bb69071204c034497821a8e7c
Aditya Khanna <khanna7@uchicago.edu>
2015-01-12 15:37:25 -0600

    update testing code

--- test_keyplayer.R
+++ test_keyplayer.R
@@ -10,7 +10,7 @@
   source("keyplayer.R")
 
   ## Trial datasets
-  gdata(flo)
+  data(flo)
   flo.net <- as.network(flo, directed=FALSE)
   flo.net  
   gplot(flo.net,
@@ -24,10 +24,10 @@
   ## Test function
   list.input <- list(rep(0, 5))
   list.input[[1]] <- flo.net
-  list.input[[2]] <- 3
+  list.input[[2]] <- 2
   list.input[[3]] <- 0.001
   list.input[[4]] <- 10
-  list.input[[5]] <- 9
+  list.input[[5]] <- 3
 
   main(list.input)
   
