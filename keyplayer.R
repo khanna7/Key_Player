6a7698e96bb8b3c08692286157f0ee97f965a618
Simon Jacobs <sdjacobs@uchicago.edu>
2015-01-14 11:08:22 -0600

    Did some refactoring, a bugfix, and implemented metric 14 [kpp-pos]

--- keyplayer.R
+++ keyplayer.R
@@ -23,52 +23,70 @@ reachability = function(A) {
     return ((D > 0) & (D < Inf));
 }
 
+# trim array based on True, False mask (see get_random_mask above)
+trimmed_array = function(A, s) {
+    B <- A[s,][,s]
+    return(B);
+}
+
+# A *metric* is a function that takes an adjancancy matrix G an indicator vector s, where:
+#   s = [s1, s2, s3, .. sn] where s_i = TRUE iff node i in S
+# and returns a number between 0 and 1.
 
 # KPP-Neg metric:
 
 # number of disconnected pairs (Borgatti (3))
 # maximize D =  1 - 2 * sum(i>j) R_ij / (n * (n-1))
 # Equation (4) should be more tractable in the optimization context (although lose out on elegance of equation)
-metric_3 <- function(A) {
+metric_3 <- function(G, s) {
+    A <- trimmed_array(G, !s)
     n <- length(A[1,])
     R <- reachability(A);
-    s <- sum(R[lower.tri(R)]);
-    return(1 - 2*s/(n * (n-1)));
+    S <- sum(R[lower.tri(R)]);
+    return(1 - 2*S/(n * (n-1)));
 }
 
 # Borghatti (9)
 # maximize D_F = 1 - 2 * (sum(i>j) (1 / d_ij) / (n * (n-1))
-metric_9 <- function(A) {
+metric_9 <- function(G, s) {
+    A <- trimmed_array(G, !s)
     n <- length(A[1,])
     D <- graph_distance(A);
     Dr <- 1/D
-    s <- sum(Dr[lower.tri(D)]);
-    return(1 - 2*s/(n * (n-1)));
+    S <- sum(Dr[lower.tri(D)]);
+    return(1 - 2*S/(n * (n-1)));
 }
+
+# KPP-Pos metric:
+
+# Borghatti (14)
+# d_Kj = min { d(i, j) where i in K, j in G\S }
+# D_R = sum(j) (1/d_Kj) / n
+metric_14 <- function(G, s) {
+    n <- length(G[1,])
+    D <- graph_distance(G) # future optimization: only do the distance calculation ONE time.
+
+    H <- apply(D[!s, s], 1, min); # 0 < d <= Inf 
+    return (sum(1/H)/n)
+}
+
 
 # return something like [TRUE, FALSE, FALSE, TRUE, ...] where there are n total values and k FALSEs
 get_random_mask = function(n, k) {
     s <- sample(n, k);
-    t <- !1:n %in% s;
-    return (t);
-}
-
-# trim array based on True, False mask (see get_random_mask above)
-trimmed_array = function(A, m) {
-    i <- which(m)
-    B <- A[i,][,i]
-    return(B);
+    s <- 1:n %in% s;
+    return (s);
 }
 
 
 greedy_optimize = function(A, k, metric, tolerance) {
     n <- length(A[1,])
 
-    t <- get_random_mask(n, k); # random index matrix to start with.
-                                # our "target" nodes are {i | t[i] is FALSE}
+    s <- get_random_mask(n, k); # random index matrix to start with.
+                                # our "target" nodes are {i | s[i] is TRUE}
 
-    B <- trimmed_array(A, t);
-    fit <- metric(B)
+
+    fit <- metric(A, s)
 
     print(paste("Fit: ", fit))
     i <- 0
@@ -76,14 +94,13 @@ greedy_optimize = function(A, k, metric, tolerance) {
         i <- i + 1
         Dfit = 0
         pair = NULL
-        for (u in which(!t)) {
-            for (v in which(t)) {
-                t_ <- t # clone
-                t_[v] = FALSE;
-                t_[u] = TRUE;
+        for (u in which(s)) {
+            for (v in which(!s)) {
+                s_ <- s # clone
+                s_[v] = TRUE;
+                s_[u] = FALSE;
                 
-                B_ = trimmed_array(A, t_)
-                fit_ = metric(B_)
+                fit_ = metric(A, s_)
                 d = fit_ - fit
                 if ((d >= 0) && (d > Dfit)) {
                     Dfit = d
@@ -91,17 +108,18 @@ greedy_optimize = function(A, k, metric, tolerance) {
                 }
             }
         }
-        if (Dfit < tolerance || is.null(pair))
+        if (Dfit < tolerance)
             break
+        print(paste("Iteration",i,":", fit, "=>",fit+Dfit))
         u <- pair[[1]]
         v <- pair[[2]]
-        t[v] = FALSE
-        t[u] = TRUE
-        fit = fit + d
+        s[v] = TRUE
+        s[u] = FALSE
+        fit = fit + Dfit
     }
 
     print(paste("New fit (", i, " iterations): ", fit));
-    return(which(!t));
+    return(which(s));
 }
 
 
@@ -118,8 +136,10 @@ main = function(argv) {
         metric <- metric_3
     else if (m == 9)
         metric <- metric_9
+    else if (m == 14)
+        metric <- metric_14
     else {
-        print("Invalid metric! Use 3 or 9 only.")
+        print("Invalid metric! Use 3, 9, or 14 only.")
         q()
     }
 
